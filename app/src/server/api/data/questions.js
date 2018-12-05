2306022ba59f81ad7fb6fde81dfd358ef313da3a
Jens Elstner <jens.elstner@technicallyfeasible.com>
2018-12-05 11:58:39 +0200

    feat(question): question ui, cleanup question answers

--- app/src/server/api/data/questions.js
+++ app/src/server/api/data/questions.js
@@ -1,219 +1,258 @@
 export default [
   {
     title: 'How many times have you had a urinary tract infection (UTI)? (Please only count UTIs that were diagnosed by a urine test and treated with antibiotics.)',
-    answers: ['Never', 'Once', 'Twice', 'Three', 'times', 'Four', 'or', 'more', 'times', 'I\'m', 'not', 'sure'],
+    answers: ['Never', 'Once', 'Twice', 'Three times', 'Four or more times', 'I\'m not sure'],
   },
   {
     title: 'Did your mother smoke while she was pregnant with you?',
-    answers: ['Yes,', 'regularly', 'Yes,', 'occasionally', 'Yes,', 'but', 'I', 'am', 'not', 'sure', 'how', 'frequently', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes, regularly', 'Yes, occasionally', 'Yes, but I am not sure how frequently', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you cry easily?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure']
+    answers: ['Yes', 'No', 'I\'m not sure']
   },
   {
     title: 'Have you ever had allergic rhinitis (stuffed or dripping nose caused by allergies)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Which of the following best describes how close you live to the nearest farm fields? Your best guess is fine.',
-    answers: ['A', 'quarter', 'of', 'a', 'mile', '(0.4', 'km)', 'or', 'less', 'More', 'than', 'a', 'quarter', 'of', 'a', 'mile', '(0.4', 'km)', 'but', 'less', 'than', '1', 'mile', '(1.6', 'km)', 'More', 'than', '1', 'mile', '(1.6', 'km)', 'but', 'less', 'than', '5', 'miles', '(8', 'km)', '5', 'miles', '(8', 'km)', 'or', 'more', 'I\'m', 'not', 'sure'],
+    answers: ['A quarter of a mile', '(0.4', 'km)', 'or', 'less', 'More', 'than', 'A quarter of a mile', '(0.4', 'km)', 'but', 'less', 'than', '1', 'mile', '(1.6', 'km)', 'More', 'than', '1', 'mile', '(1.6', 'km)', 'but', 'less', 'than', '5', 'miles', '(8', 'km)', '5', 'miles', '(8', 'km)', 'or', 'more', 'I\'m not sure'],
   },
   {
     title: 'Do you have an os trigonum (extra bone behind your ankle bone)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How many languages do you speak fluently?',
-    answers: ['1', '2', '3', '4', '5', '6', 'or', 'more', 'I\'m', 'not', 'sure'],
+    answers: ['1', '2', '3', '4', '5', '6', 'or', 'more', 'I\'m not sure'],
   },
   {
     title: 'Were you born with impaired hearing?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How often do you feel isolated from others?',
-    answers: ['Never', 'Some', 'of', 'the', 'time', 'Half', 'of', 'the', 'time', 'Most', 'of', 'the', 'time', 'Always'],
+    answers: ['Never', 'Some of the time', 'Half of the time', 'Most of the time', 'Always'],
   },
   {
     title: 'In general, people often face risks when making financial, career, or other life decisions. Overall, do you feel comfortable or uncomfortable taking risks?',
-    answers: ['Very', 'comfortable', 'taking', 'risks', 'Somewhat', 'comfortable', 'taking', 'risks', 'Neither', 'comfortable', 'nor', 'uncomfortable', 'taking', 'risks', 'Somewhat', 'uncomfortable', 'taking', 'risks', 'Very', 'uncomfortable', 'taking', 'risks'],
+    answers: [
+      'Very comfortable taking risks',
+      'Somewhat comfortable taking risks',
+      'Neither comfortable nor uncomfortable taking risks',
+      'Somewhat uncomfortable taking risks',
+      'Very uncomfortable taking risks',
+    ],
   },
   {
     title: 'Have you ever carried an epinephrine autoinjector (such as an EpiPen) for use on yourself to treat a severe allergic reaction or anaphylaxis?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been told by a medical professional that your HDL (so-called good cholesterol) is too low?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you have acrophobia (fear of heights)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Excluding statistics courses, what is the most advanced math class you have successfully completed?',
-    answers: ['Pre-algebra', 'Algebra', 'Geometry', 'Trigonometry', 'Pre-calculus', 'Calculus', '(differential', 'or', 'integral', 'calculus)', 'Vector', 'calculus', 'More', 'than', 'vector', 'calculus', 'I\'m', 'not', 'sure'],
+    answers: [
+      'Pre-algebra',
+      'Algebra',
+      'Geometry',
+      'Trigonometry',
+      'Pre-calculus',
+      'Calculus (differential or integral calculus)',
+      'Vector calculus',
+      'More than vector calculus',
+      'I\'m not sure',
+    ],
   },
   {
     title: 'Do you take a multivitamin most days?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you have a good sense of direction?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'When you\'re in the mood for a snack, what kind of snack do you usually reach for?',
-    answers: ['Sweet', 'Salty', 'or', 'savory', 'Both', 'Neither'],
+    answers: ['Sweet', 'Salty or savory', 'Both', 'Neither'],
   },
   {
     title: 'How often do you suffer from back pain?',
-    answers: ['Very', 'often', 'Fairly', 'often', 'Sometimes', 'Occasionally', 'Never', 'or', 'almost', 'never', 'I\'m', 'not', 'sure'],
+    answers: ['Very often', 'Fairly often', 'Sometimes', 'Occasionally', 'Never or almost never', 'I\'m not sure'],
   },
   {
     title: 'How often do you feel left out of social activity?',
-    answers:', '['Never', 'Some', 'of', 'the', 'time', 'Half', 'of', 'the', 'time', 'Most', 'of', 'the', 'time', 'Always'],
+    answers: ['Never', 'Some of the time', 'Half of the time', 'Most of the time', 'Always'],
   },
   {
     title: 'During social gatherings, how often do you like being the center of attention?',
-    answers: ['Always', 'Most', 'of', 'the', 'time', 'About', 'half', 'the', 'time', 'Some', 'of', 'the', 'time', 'Never'],
+    answers: ['Always', 'Most of the time', 'About half the time', 'Some of the time', 'Never'],
   },
   {
     title: 'Can you clap in time with a musical beat?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you take pleasure in organizing your stuff, such as your papers, clothes, or books?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Does eating dark chocolate typically cause you to sneeze?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been diagnosed with cluster headaches?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you had your appendix removed?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Are you a vegan (someone who does not eat any animal products, including eggs and milk)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How many times have you had pneumonia?',
-    answers: ['Never', 'Once', 'Twice', 'Three', 'times', 'Four', 'or', 'more', 'times', 'I\'m', 'not', 'sure'],
+    answers: ['Never', 'Once', 'Twice', 'Three times', 'Four or more times', 'I\'m not sure'],
   },
   {
     title: 'Have you ever had allergic conjunctivitis (red, itchy, or painful eyes caused by allergies)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'When you are sitting, how often do you jiggle your leg for no apparent reason?',
-    answers: ['Very', 'Frequently', 'Frequently', 'Sometimes', 'Rarely', 'Never', 'I\'m', 'not', 'sure'],
+    answers: ['Very Frequently', 'Frequently', 'Sometimes', 'Rarely', 'Never', 'I\'m not sure'],
   },
   {
     title: 'When asked, can you sing back any note you just heard?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Were you born with an undescended testicle or testicles?',
-    answers: ['Yes', 'No', 'I\'d', 'rather', 'not', 'say'],
+    answers: ['Yes', 'No', 'I\'d rather not say'],
   },
   {
     title: 'Are you nearsighted (near objects are clear, far objects are blurry)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'If forced to choose, would you consider yourself to be more cautious or more adventurous?',
-    answers: ['Very', 'cautious', 'Somewhat', 'cautious', 'Neither', 'cautious', 'nor', 'adventurous', 'Somewhat', 'adventurous', 'Very', 'adventurous', 'I\'m', 'not', 'sure'],
+    answers: [
+      'Very cautious',
+      'Somewhat cautious',
+      'Neither cautious nor adventurous',
+      'Somewhat adventurous',
+      'Very adventurous',
+      'I\'m not sure',
+    ],
   },
   {
     title: 'Do you like the taste of fresh cucumbers?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Which of the following best describes how close you live to the nearest industrial facility, factory, or power plant? Your best guess is fine.',
-    answers: ['A', 'quarter', 'of', 'a', 'mile', '(0.4', 'km)', 'or', 'less', 'More', 'than', 'a', 'quarter', 'of', 'a', 'mile', '(0.4', 'km)', 'but', 'less', 'than', '1', 'mile', '(1.6', 'km)', 'More', 'than', '1', 'mile', '(1.6', 'km)', 'but', 'less', 'than', '5', 'miles', '(8', 'km)', '5', 'miles', '(8', 'km)', 'or', 'more', 'I\'m', 'not', 'sure'],
+    answers: [
+      'A quarter of a mile (0.4 km) or less',
+      'More than A quarter of a mile (0.4 km) but less than 1 mile (1.6 km)',
+      'More than 1 mile (1.6 km) but less than 5 miles (8 km)',
+      '5 miles (8 km) or more',
+      'I\'m not sure',
+    ],
   },
   {
     title: 'On most nights, do you snore?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How often do you have dandruff? (If you use treatments for your dandruff, please answer for the frequency of occurrence without treatment.)',
-    answers: ['Always', 'or', 'almost', 'always', 'Often', 'Sometimes', 'Rarely', 'Never', 'or', 'almost', 'never', 'I\'m', 'not', 'sure'],
+    answers: ['Always or almost always', 'Often', 'Sometimes', 'Rarely', 'Never or almost never', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been diagnosed with gallstones?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you have a fear of public speaking?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been diagnosed with a lipoma (a benign tumor composed of fatty tissue)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you have agoraphobia (fear of public places, crowds, or open spaces)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'When you feel mentally drained, do you prefer to recharge your batteries by...',
-    answers: ['Spending', 'time', 'with', 'friends', 'or', 'family', 'Spending', 'time', 'alone', 'No', 'strong', 'preference', 'either', 'way', 'I\'m', 'not', 'sure'],
+    answers: [
+      'Spending time with friends or family',
+      'Spending time alone',
+      'No strong preference either way',
+      'I\'m not sure',
+    ],
   },
   {
     title: 'If you were to select a shampoo for your hair type, which shampoo would you select?',
-    answers: ['A', 'shampoo', 'for', 'oily', 'hair', 'A', 'shampoo', 'for', 'normal', 'hair', 'A', 'shampoo', 'for', 'dry', 'hair', 'I\'m', 'not', 'sure'],
+    answers: [
+      'A shampoo for oily hair',
+      'A shampoo for normal hair',
+      'A shampoo for dry hair',
+      'I\'m not sure',
+    ],
   },
   {
     title: 'Are you exposed to second-hand smoke in your home?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you had your tonsils removed?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been diagnosed with Lou Gehrig\'s disease (ALS)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Were you adopted?',
-    answers: ['Yes', 'No', 'I\'d', 'rather', 'not', 'say'],
+    answers: ['Yes', 'No', 'I\'d rather not say'],
   },
   {
     title: 'Do you use a hearing aid?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'In a typical month are any pesticides, including herbicides (to kill weeds), fungicides (to kill fungus/mold), insecticides (to kill insects), rodenticides (to kill rats/mice), or fumigants (gas used to kill insects or fungus or plants) used in your home and garden, or on your pet?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Do you have stretch marks on your hips, thighs, or backs of your arms?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Are you naturally a night person or a morning person?',
-    answers: ['Night', 'person', 'Morning', 'person', 'Neither', 'It', 'depends', 'I\'m', 'not', 'sure'],
+    answers: ['Night', 'person', 'Morning', 'person', 'Neither', 'It', 'depends', 'I\'m not sure'],
   },
   {
     title: 'Has a doctor ever told you that you had abnormally low testosterone?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever received chemotherapy?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever lost 20 lbs. (10 kg.) or more and kept the weight off for at least one year?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever had a stammer or stutter?',
@@ -221,71 +260,84 @@ export default [
   },
   {
     title: 'Do you have an identical twin?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Is your desk typically...',
-    answers: ['Very', 'clean', 'Fairly', 'clean', 'Fairly', 'messy', 'Very', 'messy', 'I\'m', 'not', 'sure'],
+    answers: ['Very clean', 'Fairly clean', 'Fairly messy', 'Very messy', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been diagnosed with a chromosomal abnormality?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How would you rate your mathematical ability?',
-    answers: ['Excellent', 'Good', 'About', 'average', 'Poor', 'Very', 'poor', 'I\'m', 'not', 'sure'],
+    answers: ['Excellent', 'Good', 'About average', 'Poor', 'Very poor', 'I\'m not sure'],
   },
   {
     title: 'How many times have you been to see a physician in the past 12 months, for any health issue?',
-    answers: ['0', '1', '2', '3', 'or', 'more'],
+    answers: ['0', '1', '2', '3 or more'],
   },
   {
     title: 'Have you ever taken a non-steroidal anti-inflammatory drug (such as ibuprofen, naproxen, or aspirin) on a regular basis?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Are you exposed to second-hand smoke at work?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Have you ever been able to do the forward splits (one leg to the front, and one leg behind)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'When you were a child, did you ever have febrile seizures?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Are you out of breath after walking up a flight of stairs?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How satisfied are you with your life?',
-    answers: ['Very', 'dissatisfied', 'Somewhat', 'dissatisfied', 'Neither', 'satisfied', 'nor', 'dissatisfied', 'Somewhat', 'satisfied', 'Very', 'satisfied'],
+    answers: [
+      'Very dissatisfied',
+      'Somewhat dissatisfied',
+      'Neither satisfied nor dissatisfied',
+      'Somewhat satisfied',
+      'Very satisfied',
+    ],
   },
   {
     title: 'If forced to choose, would you say you generally prefer to talk or to listen?',
-    answers: ['Talk', 'Listen', 'I\'m', 'not', 'sure'],
+    answers: ['Talk', 'Listen', 'I\'m not sure'],
   },
   {
     title: 'Did you have several ear infections as a child (under the age of 18)?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Did your family live within a quarter of a mile (0.4 km) of farm fields before you were four years old?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'Someone cuts you off on a motorway. How irritated are you?',
-    answers: ['Furious', 'Very', 'irritated', 'Fairly', 'irritated', 'A', 'little', 'irritated', 'Not', 'at', 'all', 'I\'m', 'not', 'sure'],
+    answers: [
+      'Furious',
+      'Very irritated',
+      'Fairly irritated',
+      'A little irritated',
+      'Not at all',
+      'I\'m not sure',
+    ],
   },
   {
     title: 'Have you ever been diagnosed with high cholesterol?',
-    answers: ['Yes', 'No', 'I\'m', 'not', 'sure'],
+    answers: ['Yes', 'No', 'I\'m not sure'],
   },
   {
     title: 'How many times have you been hospitalised in the past 12 months?',
-    answers: ['0', '1', '2', '3', 'or', 'more'],
+    answers: ['0', '1', '2', '3 or more'],
   },
   {
     title: 'Have you ever taken warfarin (Marevan)?',
